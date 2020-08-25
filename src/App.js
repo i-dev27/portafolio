13e965dc272bd11db5e284e72011e1ded089e079
Irving Rabanales <irvmr88@gmail.com>
2020-08-24 19:48:23 -0600

    routes&thumbnails

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import Projects from "./Projects.jsx";
+import Articles from "./Articles.jsx";
+import "./App.css";
+import { Link, BrowserRouter, Route } from "react-router-dom";
+import About from "./About.jsx";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Route exact path="/" component={Projects} />
+        <Route path="/articles" component={Articles} />
+        <Route path="/about" component={About} />
+
+        <div className="navigation">
+          <img src={logo} alt="Logo Image" className="logo" />
+          <div className="navigation-sub">
+            <Link to="/" className="item">
+              Projects
+            </Link>
+            <Link to="/articles" className="item">
+              Articles
+            </Link>
+            <Link to="/about" className="item">
+              About
+            </Link>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import Thumbnail from "./Thumbnail";
 
 function Projects(props) {
   return (
     <div>
-      <h1>New Component</h1>
+      <h1>Projects</h1>
+      <Thumbnail
+        link="/twitter"
+        image="http://twitter-image-url.jpg"
+        title="Twitter Newsfeed"
+        category="Mobile App"
+      />
+      <Thumbnail
+        link="/twitter"
+        image="http://twitter-image-url.jpg"
+        title="Twitter Newsfeed"
+        category="Mobile App"
+      />
+      <Thumbnail
+        link="/twitter"
+        image="http://twitter-image-url.jpg"
+        title="Twitter Newsfeed"
+        category="Mobile App"
+      />
     </div>
   );
 }
