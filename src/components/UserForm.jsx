e78aa288aafad4f4c85e7ea4c120db05cef2e5cb
Irving Rabanales <irvmr88@gmail.com>
2020-09-24 22:47:36 -0600

    content-8thversion

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -14,20 +14,34 @@ import {
   Container,
   createMuiTheme,
   ThemeProvider,
+  Box,
 } from "@material-ui/core";
 import { green } from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "50%",
+    padding: 0,
+    margin: 0,
+    display: "block",
+    justifyContent: "flexStart",
+    backgroundColor: "#d3d3d3",
+  },
+
   margin: {
     margin: theme.spacing(2),
-  },
-  background: {
-    backgroundColor: "#d3d3d3",
   },
 
   TextField: {
     width: "36%",
     margin: "10px",
+  },
+
+  infoBox: {
+    backgroundColor: "grey",
+    display: "flex",
+    justifyContent: "flexEnd",
+    alignItems: "center",
   },
 }));
 
@@ -42,56 +56,66 @@ const theme = createMuiTheme({
 export default function UserForm() {
   const classes = useStyles();
   return (
-    <Container className={classes.background}>
-      <h1>Contact us</h1>
-      <Grid>
+    <React.Fragment>
+      <Container className={classes.root}>
+        <h1>Contact us</h1>
         <Grid>
-          <FormControl className={classes.margin}>
-            <InputLabel htmlFor="input-with-icon-adornment">Nombre</InputLabel>
-            <Input
-              id="input-with-icon-adornment"
-              startAdornment={
-                <InputAdornment position="start">
-                  <AccountCircle />
-                </InputAdornment>
-              }
+          <Grid>
+            <FormControl className={classes.margin}>
+              <InputLabel htmlFor="input-with-icon-adornment">
+                Nombre
+              </InputLabel>
+              <Input
+                id="input-with-icon-adornment"
+                startAdornment={
+                  <InputAdornment position="start">
+                    <AccountCircle />
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            <TextField
+              className={classes.margin}
+              id="input-with-icon-textfield"
+              label="Email"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <AlternateEmailIcon />
+                  </InputAdornment>
+                ),
+              }}
             />
-          </FormControl>
-          <TextField
-            className={classes.margin}
-            id="input-with-icon-textfield"
-            label="Email"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <AlternateEmailIcon />
-                </InputAdornment>
-              ),
-            }}
-          />
+          </Grid>
         </Grid>
-      </Grid>
-      <Grid>
-        <TextField
-          className={classes.TextField}
-          id="outlined-multiline-static"
-          label="Comentario"
-          multiline
-          rows={4}
-          variant="outlined"
-        />
         <Grid>
-          <ThemeProvider theme={theme}>
-            <Button
-              variant="contained"
-              color="primary"
-              endIcon={<Icon>send</Icon>}
-            >
-              Send
-            </Button>
-          </ThemeProvider>
+          <TextField
+            className={classes.TextField}
+            id="outlined-multiline-static"
+            label="Comentario"
+            multiline
+            rows={4}
+            variant="outlined"
+          />
+          <Grid>
+            <ThemeProvider theme={theme}>
+              <Button
+                variant="contained"
+                color="primary"
+                endIcon={<Icon>send</Icon>}
+              >
+                Send
+              </Button>
+            </ThemeProvider>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+
+      <Container className={classes.infoBox}>
+        <Grid direction="row" justify="flex-end" alignItems="flex-end">
+          <h1>hola</h1>
+        </Grid>
+      </Container>
+    </React.Fragment>
   );
 }
